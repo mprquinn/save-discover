a75c4579faae6a1fca362c900dab90d5ed46cd2e
Mike <mprquinn@gmail.com>
2018-02-05 16:03:01 -0700

    Filter changes all data now

--- src/App.js
+++ src/App.js
@@ -63,29 +63,28 @@ class App extends Component {
     // }, 2000);
   }
   render() {
+    let playlistsToRender = this.state.serverData.user ? this.state.serverData.user.playlists.filter(playlist => 
+      playlist.name.toLowerCase().includes(
+        this.state.filterString.toLowerCase()
+      )
+    ) : [];
     return (
       <div className="App">
         {this.state.serverData.user ?
           <div>{/* Love you, React */}
             <Title username={this.state.serverData.user && this.state.serverData.user.name} />
 
-            <PlaylistCounter playlists={this.state.serverData.user && this.state.serverData.user.playlists} />
+            <PlaylistCounter playlists={playlistsToRender} />
 
-            <HourCounter playlists={this.state.serverData.user && this.state.serverData.user.playlists} />
+            <HourCounter playlists={playlistsToRender} />
 
             <Filter onTextChange={(text) => this.setState({ filterString: text })} />
 
-            {this.state.serverData.user.playlists.filter(playlist => {
-              return playlist.name.toLowerCase().includes(
-                this.state.filterString.toLowerCase()
-              )
-            })
+            {playlistsToRender
               .map(playlist =>
                 <Playlist key={playlist.name} playlist={playlist} />
               )}
-            {/* <Playlist />
-            <Playlist />
-            <Playlist /> */}
+
           </div> : <p className="loading">Loading...</p>
         }
       </div>

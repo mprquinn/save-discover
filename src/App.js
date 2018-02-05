0805221512870366dccf5f067f49eb7ee02f6045
Mike <mprquinn@gmail.com>
2018-02-05 12:10:45 -0700

    Dummy app setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Title from './components/Title';
+import Aggregate from './components/Aggregate';
+import Filter from './components/Filter';
+import Playlist from './components/Playlist';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Title />
+        <Aggregate />
+        <Aggregate />
+        <Filter />
+        <Playlist />
+        <Playlist />
+        <Playlist />
       </div>
     );
   }

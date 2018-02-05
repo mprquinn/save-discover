3e71363b3ab552b251cc31047f6221468e420ba0
Mike <mprquinn@gmail.com>
2018-02-05 15:52:49 -0700

    clean up the app file

--- src/App.js
+++ src/App.js
@@ -14,27 +14,27 @@ let fakeServerData = {
       {
         name: 'Favourites',
         songs: [
-          {name: 'A Song', duration: 210},
-          {name: 'A Fake Song', duration: 300},
-          {name: 'This is a Tune', duration: 240},
-          {name: 'Im a Song (Feat. Singer)', duration: 330}
+          { name: 'A Song', duration: 210 },
+          { name: 'A Fake Song', duration: 300 },
+          { name: 'This is a Tune', duration: 240 },
+          { name: 'Im a Song (Feat. Singer)', duration: 330 }
         ]
       },
       {
         name: 'Discover Weekly',
         songs: [
-          {name: 'DW - A Song', duration: 180},
-          {name: 'Ive Discovered this Song', duration: 240},
-          {name: 'Another Song', duration: 460},
-          {name: 'Heyo', duration: 390}
+          { name: 'DW - A Song', duration: 180 },
+          { name: 'Ive Discovered this Song', duration: 240 },
+          { name: 'Another Song', duration: 460 },
+          { name: 'Heyo', duration: 390 }
         ]
       },
       {
         name: 'Release Radar',
         songs: [
-          {name: 'Newly Released Song', duration: 520},
-          {name: 'New Hotness', duration: 180},
-          {name: 'This Song is a Banger', duration: 370}
+          { name: 'Newly Released Song', duration: 520 },
+          { name: 'New Hotness', duration: 180 },
+          { name: 'This Song is a Banger', duration: 370 }
         ]
       }
     ]
@@ -68,17 +68,21 @@ class App extends Component {
         {this.state.serverData.user ?
           <div>{/* Love you, React */}
             <Title username={this.state.serverData.user && this.state.serverData.user.name} />
+
             <PlaylistCounter playlists={this.state.serverData.user && this.state.serverData.user.playlists} />
+
             <HourCounter playlists={this.state.serverData.user && this.state.serverData.user.playlists} />
-            <Filter onTextChange={(text) => this.setState({filterString: text})}/>
+
+            <Filter onTextChange={(text) => this.setState({ filterString: text })} />
+
             {this.state.serverData.user.playlists.filter(playlist => {
               return playlist.name.toLowerCase().includes(
                 this.state.filterString.toLowerCase()
               )
             })
-            .map(playlist =>
-              <Playlist key={playlist.name} playlist={playlist}/>
-            )}
+              .map(playlist =>
+                <Playlist key={playlist.name} playlist={playlist} />
+              )}
             {/* <Playlist />
             <Playlist />
             <Playlist /> */}

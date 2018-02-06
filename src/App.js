d20814fe6d36c3050184cb79c75c8aebf64b29ed
Mike <mprquinn@gmail.com>
2018-02-06 09:12:36 -0700

    code cleanup

--- src/App.js
+++ src/App.js
@@ -20,113 +20,119 @@ class App extends Component {
   }
   handleErrors(response) {
     if (!response.ok) {
-      throw Error('test',response);
+      throw Error("test", response);
     }
     return response;
   }
   fetchData() {
     let query = queryString.parse(window.location.search);
     const parsed = query.access_token;
-    if (!parsed) {return;}
+    if (!parsed) {
+      return;
+    }
     fetch("https://api.spotify.com/v1/me", {
       headers: {
         Authorization: `Bearer ${parsed}`
       }
     })
-    .then(this.handleErrors)
-    .then(response => {
+      .then(this.handleErrors)
+      .then(response => {
         return response.json();
-    }).then(data => {
-      if (data.id !== undefined) {
-        this.setState({
-          loggedIn: true,
-          user: {
-            name: data.display_name ? data.display_name : data.id,
-            uid: data.id,
-            profile_picture: data.images[0].url
-          },
-        });
-      }
-    }).catch(error => console.log(error));
+      })
+      .then(data => {
+        if (data.id !== undefined) {
+          this.setState({
+            loggedIn: true,
+            user: {
+              name: data.display_name ? data.display_name : data.id,
+              uid: data.id,
+              profile_picture: data.images[0].url
+            }
+          });
+        }
+      })
+      .catch(error => console.log(error));
 
     fetch("https://api.spotify.com/v1/me/playlists", {
       headers: {
         Authorization: `Bearer ${parsed}`
       }
     })
-    .then(this.handleErrors)
-    .then(response => {
-      return response.json();
-    })
-    .then(playlistData => {
-      let playlists = playlistData.items.filter(item => {
-        if (!item.tracks.href.split('/users/')[1].includes('spotify')) {
-          return true;
-        } else {
-          return false;
+      .then(this.handleErrors)
+      .then(response => {
+        return response.json();
+      })
+      .then(playlistData => {
+        let playlists = playlistData.items.filter(item => {
+          if (!item.tracks.href.split("/users/")[1].includes("spotify")) {
+            return true;
+          } else {
+            return false;
+          }
+        });
+        let trackDataPromises = playlists.map(playlist => {
+          let responsePromise = fetch(playlist.tracks.href, {
+            headers: { Authorization: `Bearer ${parsed}` }
+          });
+          let trackDataPromise = responsePromise.then(response =>
+            response.json()
+          );
+          return trackDataPromise;
+        });
+        let allTrackDataPromises = Promise.all(trackDataPromises);
+        let playlistsPromise = allTrackDataPromises.then(tracksData => {
+          tracksData.forEach((trackData, i) => {
+            playlists[i].tracks = trackData.items.map(item => item.track);
+          });
+          return playlists;
+        });
+        return playlistsPromise;
+      })
+      .then(playlists => {
+        if (playlists !== undefined) {
+          this.setState({
+            playlists: playlists
+              .filter(playlist => {
+                if (playlist.name !== null) {
+                  return true;
+                } else {
+                  return false;
+                }
+              })
+              .map(item => {
+                return {
+                  name: item.name,
+                  songs: item.tracks.map(trackData => ({
+                    name: trackData.name,
+                    duration: trackData.duration_ms / 1000
+                  })),
+                  image: item.images[0].url
+                };
+              })
+          });
         }
-      });
-      let trackDataPromises = playlists.map(playlist => {
-        let responsePromise = fetch(playlist.tracks.href, {
-          headers: { Authorization: `Bearer ${parsed}` }
-        })
-        let trackDataPromise = responsePromise.then(response => response.json());
-        return trackDataPromise;
-      });
-      let allTrackDataPromises = Promise.all(trackDataPromises);
-      let playlistsPromise = allTrackDataPromises.then(tracksData => {
-        tracksData.forEach((trackData, i) => {
-          playlists[i].tracks = trackData.items.map(item => item.track);
-        });
-        return playlists;
-      });
-      return playlistsPromise;
-    })
-    .then(playlists => {
-      if (playlists !== undefined) {
-        this.setState({
-          playlists: playlists.filter((playlist) => {
-            if (playlist.name !== null) {
-              return true;
-            } else {
-              return false;
-            }
-          }).map((item) => {
-            return {
-              name: item.name, 
-              songs: item.tracks.map(trackData => ({
-                name: trackData.name,
-                duration: trackData.duration_ms / 1000
-              })),
-              image: item.images[0].url
-            }
-          })
-        });
-      }
-    }).catch(error => console.log(error));
+      })
+      .catch(error => console.log(error));
   }
   componentDidMount() {
     this.fetchData();
   }
   render() {
-  let playlistsToRender =
-    this.state.user &&
-    this.state.playlists
-      ? this.state.playlists.filter(playlist =>
-          playlist.name
-            .toLowerCase()
-            .includes(this.state.filterString.toLowerCase())
-        )
-      : [];
+    let playlistsToRender =
+      this.state.user && this.state.playlists
+        ? this.state.playlists.filter(playlist =>
+            playlist.name
+              .toLowerCase()
+              .includes(this.state.filterString.toLowerCase())
+          )
+        : [];
     return (
       <div className="App">
         {this.state.loggedIn ? (
           <div>
             {/* Love you, React */}
             <Title
-              username={
-                this.state.user && this.state.user.name
-              }
+              username={this.state.user && this.state.user.name}
               profile_picture={this.state.user.profile_picture}
             />
 
@@ -139,15 +145,19 @@ class App extends Component {
             />
 
             {playlistsToRender.map(playlist => (
-              <Playlist key={playlist.name} playlist={playlist} image={playlist.image}/>
+              <Playlist
+                key={playlist.name}
+                playlist={playlist}
+                image={playlist.image}
+              />
             ))}
           </div>
         ) : (
           <div className="log-in-wrapper">
             <button
-              onClick={() =>{
-                if (window.location.href.includes('localhost')) {
-                  window.location = `http://localhost:8888/login`
+              onClick={() => {
+                if (window.location.href.includes("localhost")) {
+                  window.location = `http://localhost:8888/login`;
                 } else {
                   window.location = `https://save-discover-backend.herokuapp.com/login`;
                 }

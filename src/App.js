49652fd31a3c5e304c6f9f1cd0cc918e65b76714
Mike <mprquinn@gmail.com>
2018-02-06 11:19:58 -0700

    fix headers

--- src/App.js
+++ src/App.js
@@ -95,19 +95,20 @@ class App extends Component {
           return false;
         }
       });
-      toBuild[0].name = 'TEST TEST TEST';
-      console.log(toBuild, accessToken);
-      fetch(`${slug}playlists/${this.state.user.uid}/playlists`, {
-        method: 'POST',
-        body: JSON.stringify(toBuild),
-        headers: new Headers ({
-          Authorization: `Bearer ${accessToken}`,
-          'Content-Type': 'application/json'
-        })
-      })
-      .then(response => response.json())
-      .catch(error => console.log('Error:', error))
-      .then(response => console.log('Success', response));
+      const tracks = toBuild[0].tracks;
+      // Manually create the json here since the spotify one has the wrong user info
+      
+      // fetch(`${slug}users/${this.state.user.uid}/playlists`, {
+      //   method: 'POST',
+      //   body: JSON.stringify(toBuild),
+      //   headers: new Headers ({
+      //     Authorization: `Bearer ${accessToken}`,
+      //     'Content-Type': 'application/json'
+      //   })
+      // })
+      // .then(response => response.json())
+      // .catch(error => console.log('Error:', error))
+      // .then(response => console.log('Success', response));
     }
   }
   componentDidMount() {
